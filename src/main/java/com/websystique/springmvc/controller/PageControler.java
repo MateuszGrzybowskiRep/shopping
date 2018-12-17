af49f1f168a8a698dd2f2e3bf46f18ed7131dd19
Grzybatronix <mateusz.grzybowski.warszawa@gmail.com>
2018-12-17 20:03:03 +0100

    adding Bootstrap, controller, configuration file

---
+++ src/main/java/com/websystique/springmvc/controller/PageControler.java
@@ -0,0 +1,42 @@
+package com.websystique.springmvc.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageControler {
+
+    @RequestMapping(value = {"/", "home"})
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("title", "Home");
+        mv.addObject("userClickHome", true);
+        return mv;
+    }
+
+    @RequestMapping(value = "/about")
+    public ModelAndView acout() {
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("title", "Conctact us");
+        mv.addObject("userClickAbout", true);
+        return mv;
+    }
+
+    @RequestMapping(value = "/contact")
+    public ModelAndView contact() {
+        ModelAndView mv = new ModelAndView("page");
+        mv.addObject("title", "Home");
+        mv.addObject("userClickContact", true);
+        return mv;
+    }
+
+
+}
+
+
+
+
