d7b502688fb483142a2111d571fc626e98e1127c
Grzybatronix <mateusz.grzybowski.warszawa@gmail.com>
2018-12-19 01:40:44 +0100

    /only televison bootstrap button working

--- src/main/webapp/assets/js/myapp.js
+++ src/main/webapp/assets/js/myapp.js
@@ -1,14 +1,22 @@
 <!-- /.col-lg-3 -->
 $(function() {
     switch (menu) {
-        case 'Conctact Us':
-            $('#contact').addClass('active');
-            break;
+
         case 'About Us':
             $('#about').addClass('active');
             break;
+
+        case 'Conctact Us':
+            $('#contact').addClass('active');
+            break;
+
+        case 'All Products':
+            $('#listProducts').addClass('active');
+            break;
+
         default:
-            $('#home').addClass('active');
+            $('#listProducts').addClass('active');
+            $('#a_'+menu).addClass('active');
             break;
     }
 });
